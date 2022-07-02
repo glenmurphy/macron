c07ca314c43d2ef7b2482bd638e2acfd7265e267
Glen Murphy <glen@glenmurphy.com>
2022-07-02 22:13:46 +1000

    refactoring

--- src/macron.rs
+++ src/macron.rs
@@ -3,30 +3,23 @@ use tokio::sync::mpsc::{UnboundedSender, unbounded_channel};
 use std::collections::HashSet;
 use tokio_stream::{self as stream, StreamExt};
 
+static TAP_WAIT: u64 = 50;
+
 #[derive(Copy, Clone, Debug)]
 #[allow(unused)]
 pub enum Cmd {
     Press(Key),
     Release(Key),
-    Tap(Key),
-    Engage(Key, u64),
-    Sleep(u64),
     MouseMove(i32, i32),
-    Click(Button),
-    ButtonPress(Button),
-    ButtonRelease(Button),
-    Aim(u64),
-    Scan(i32, i32, u64)
-}
-
-fn sleep(ms: u64) {
-    std::thread::sleep(std::time::Duration::from_millis(ms));
-}
-
-fn aim_at(x: usize, y: usize, sensitivity: f32) {
-    let mx = ((x as f32 + 100f32) - (2560 / 2) as f32) * sensitivity;
-    let my = ((1440 / 2) as f32 - (y as f32 + 60f32)) * sensitivity;
-    mouse_move(mx as i32, my as i32);
+    MousePress(Button),
+    MouseRelease(Button),
+    Wait(u64),
+    
+    // Compound commands:
+    Tap(Key),
+    Hold(Key, u64),
+    MouseClick(Button),
+    MouseHold(Button, u64),
 }
 
 pub struct MacronInterface {
@@ -35,57 +28,92 @@ pub struct MacronInterface {
 
 #[allow(unused)]
 impl MacronInterface {
+    pub fn start(&self) {
+        let _ = self.tx.send(MacronControl::Start);
+    }
+    pub fn stop(&self) {
+        let _ = self.tx.send(MacronControl::Stop);
+    }
     pub fn toggle(&self) {
         let _ = self.tx.send(MacronControl::Toggle);
-    }
-
-    pub fn set_aim(&self, x: usize, y: usize) {
-        let _ = self.tx.send(MacronControl::SetAim(x, y));
     }
 }
 
 #[allow(unused)]
 pub enum MacronControl {
+    Start,
+    Stop,
     Toggle,
-    SetAim(usize, usize)
 }
 
-struct Aim {
-    start : std::time::Instant,
-    duration : u128,
-}
-
-impl Aim {
-    pub fn reset(&mut self, duration: u128) {
-        self.start = std::time::Instant::now();
-        self.duration = duration;
+fn flatten(cmd_list: &Vec<Cmd>) -> Vec<Cmd> {
+    // Split any compound commands into single commands
+    let mut cmds: Vec<Cmd> = Vec::new();
+    for cmd in cmd_list {
+        match cmd {
+            Cmd::Tap(key) => {
+                cmds.push(Cmd::Press(*key));
+                cmds.push(Cmd::Wait(TAP_WAIT));
+                cmds.push(Cmd::Release(*key));
+            }
+            Cmd::Hold(key, ms) => {
+                cmds.push(Cmd::Press(*key));
+                cmds.push(Cmd::Wait(*ms));
+                cmds.push(Cmd::Release(*key));
+            }
+            Cmd::MouseClick(button) => {
+                cmds.push(Cmd::MousePress(*button));
+                cmds.push(Cmd::Wait(TAP_WAIT));
+                cmds.push(Cmd::MouseRelease(*button));
+            }
+            Cmd::MouseHold(button, ms) => {
+                cmds.push(Cmd::MousePress(*button));
+                cmds.push(Cmd::Wait(*ms));
+                cmds.push(Cmd::MouseRelease(*button));
+            }
+            _ => {
+                let _ = cmds.push(*cmd);
+            }
+        }
     }
-
-    pub fn is_aiming(&self) -> bool {
-        return self.start.elapsed().as_millis() < self.duration;
-    }
+    cmds
 }
 
-pub async fn new(cmds: Vec<Cmd>) -> MacronInterface {
+
+pub async fn new(cmd_list: Vec<Cmd>, cycle: bool) -> MacronInterface {
     let (control_tx, mut control_rx) = unbounded_channel();
+    let cmds = flatten(&cmd_list);
 
-    //let (parent_tx, parent_rx) = unbounded_channel();
     tokio::spawn(async move {
         let mut run_cmds_stream = stream::iter(&cmds);
         let mut running = false;
         let mut keys_down: HashSet<Key> = HashSet::new();
         let mut buttons_down: HashSet<Button> = HashSet::new();
-        let mut aim = Aim { start : std::time::Instant::now(), duration : 0 };
 
         loop {
             tokio::select! {
                 Some(msg) = control_rx.recv() => {
                     match msg {
+                        MacronControl::Start => {
+                            println!("starting");
+                            running = true;
+                            run_cmds_stream = stream::iter(&cmds);
+                        },
+                        MacronControl::Stop => {
+                            println!("stopping");
+                            running = false;
+
+                            for key in keys_down.iter() { release(*key); }
+                            for button in buttons_down.iter() { button_release(*button); }
+                            keys_down.clear();
+                            buttons_down.clear();
+                        },
                         MacronControl::Toggle => {
                             println!("toggling");
                             if running {
                                 println!("stopping");
                                 running = false;
+
                                 for key in keys_down.iter() { release(*key); }
                                 for button in buttons_down.iter() { button_release(*button); }
                                 keys_down.clear();
@@ -96,16 +124,11 @@ pub async fn new(cmds: Vec<Cmd>) -> MacronInterface {
                                 run_cmds_stream = stream::iter(&cmds);
                             }
                         }
-                        MacronControl::SetAim(x, y) => {
-                            if aim.is_aiming() {
-                                aim_at(x, y, 1.0f32);
-                            }
-                        }
                     }
                 }
-                maybe_c = run_cmds_stream.next(), if running && !aim.is_aiming() => {
+                maybe_c = run_cmds_stream.next(), if running => {
                     if let Some(c) = maybe_c {
-                        println!("running cmd: {:?}", &c);
+                        //println!("running cmd: {:?}", &c);
                         match c {
                             Cmd::Press(key) => {
                                 press(*key);
@@ -115,37 +138,28 @@ pub async fn new(cmds: Vec<Cmd>) -> MacronInterface {
                                 release(*key);
                                 keys_down.remove(&key);
                             }
-                            Cmd::Tap(key) => {
-                                press(*key);
-                                sleep(100);
-                                release(*key);
+                            Cmd::MouseMove(x, y) => {
+                                mouse_move(*x, *y);
                             }
-                            Cmd::Engage(key, time) => {
-                                press(*key);
-                                sleep(*time);
-                                release(*key);
-                            }
-                            Cmd::Sleep(time) => sleep(*time),
-                            Cmd::MouseMove(x, y) => mouse_move(*x, *y),
-                            Cmd::Click(button) => {
-                                button_press(*button);
-                                sleep(50);
-                                button_release(*button);
-                            }
-                            Cmd::ButtonPress(button) => {
+                            Cmd::MousePress(button) => {
                                 button_press(*button);
                                 buttons_down.insert(*button);
                             }
-                            Cmd::ButtonRelease(button) => {
+                            Cmd::MouseRelease(button) => {
                                 button_release(*button);
                                 buttons_down.remove(&button);
                             }
-                            Cmd::Aim(time) => {
-                                aim.reset(*time as u128);
+                            Cmd::Wait(time) => {
+                                // ideally we would pause the stream and use a timer
+                                // so the channels can continue working
+                                std::thread::sleep(std::time::Duration::from_millis(*time));
+                            },
+                            _ => {
+                                panic!("unexpected command: {:?}", c);
                             }
-                            _ => {}
                         }
-                    } else {
+                    } else if cycle {
+                        // Run out of commands, loop
                         run_cmds_stream = stream::iter(&cmds);
                     }
                 }
--- src/main.rs
+++ src/main.rs
@@ -6,149 +6,95 @@ use macron::Cmd;
 #[tokio::main]
 async fn main() {
     println!("Macro tool awaiting input");
-    //println!("{:?}", screen::detect(255, 0, 0, 10));
 
-    // Sunshot spam
     let afk1 = macron::new(vec![
-        Cmd::MouseMove(0, 24000),
-        Cmd::Sleep(50),
-        Cmd::MouseMove(0, -17500),
-        Cmd::Sleep(50),
+        Cmd::MouseMove(0, 24000), Cmd::Wait(50),
+        Cmd::MouseMove(0, -17100), Cmd::Wait(50),
 
         Cmd::Tap(Key::Num2),
-        Cmd::Click(Button::Left),
-        Cmd::Sleep(400),
-        Cmd::Click(Button::Left),
-        Cmd::Sleep(400),
-        Cmd::Click(Button::Left),
-        Cmd::Sleep(400),
-        Cmd::Click(Button::Left),
-        Cmd::Sleep(400),
-        Cmd::Click(Button::Left),
-        Cmd::Sleep(400),
-        Cmd::Click(Button::Left),
-        Cmd::Sleep(400),
+        Cmd::MouseClick(Button::Left), Cmd::Wait(350),
+        Cmd::MouseClick(Button::Left), Cmd::Wait(350),
+        Cmd::MouseClick(Button::Left), Cmd::Wait(350),
+        Cmd::MouseClick(Button::Left), Cmd::Wait(350),
+        Cmd::MouseClick(Button::Left), Cmd::Wait(350),
+        Cmd::MouseClick(Button::Left), Cmd::Wait(750),
         
-        Cmd::MouseMove(-1000, 0),
-        Cmd::Click(Button::Left),
-        Cmd::Sleep(400),
-        Cmd::Click(Button::Left),
-        Cmd::Sleep(400),
-        Cmd::Click(Button::Left),
-        Cmd::Sleep(400),
-        Cmd::MouseMove(2000, 0),
-        Cmd::Click(Button::Left),
-        Cmd::Sleep(400),
-        Cmd::Click(Button::Left),
-        Cmd::Sleep(400),
-        Cmd::Click(Button::Left),
-        Cmd::Sleep(400),
-        Cmd::MouseMove(-1000, 0),
+        Cmd::MouseMove(-1200, 0), Cmd::Wait(50),
 
-        Cmd::Tap(Key::W),           // Forward
-        Cmd::Sleep(100),
+        Cmd::MouseClick(Button::Left), Cmd::Wait(350),
+        Cmd::MouseClick(Button::Left), Cmd::Wait(350),
+        Cmd::MouseClick(Button::Left), Cmd::Wait(700),
+
+        Cmd::MouseMove(2200, 0), Cmd::Wait(100),
+
+        Cmd::MouseClick(Button::Left), Cmd::Wait(350),
+        Cmd::MouseClick(Button::Left), Cmd::Wait(350),
+        Cmd::MouseClick(Button::Left), Cmd::Wait(50),
+
+        Cmd::MouseMove(-1000, 0),
 
         Cmd::Tap(Key::R),
-        Cmd::Press(Key::S),   // Back
-        Cmd::Sleep(450),
-        Cmd::Release(Key::S),
-        Cmd::Sleep(1000),
-    ]).await;
+        Cmd::Tap(Key::W), Cmd::Wait(100),
+        Cmd::Hold(Key::S, 450), Cmd::Wait(1400),
+    ], true).await;
 
-    // Mostly 2, single hold of 3
     let afk2 = macron::new(vec![
-        Cmd::MouseMove(0, 24000),
-        Cmd::Sleep(50),
-        Cmd::MouseMove(0, -15000),
-        Cmd::Sleep(50),
+        Cmd::MouseMove(0, 24000), Cmd::Wait(50),
+        Cmd::MouseMove(0, -15000), Cmd::Wait(50),
 
-        Cmd::Tap(Key::Num2),
-        Cmd::Sleep(700),
-
-        Cmd::MouseMove(-3000, 500),
-        Cmd::ButtonPress(Button::Left),
-        Cmd::Sleep(850),
-        Cmd::ButtonRelease(Button::Left),
-        Cmd::Sleep(600),
-
-        Cmd::MouseMove(1500, -250),
-        Cmd::ButtonPress(Button::Left),
-        Cmd::Sleep(850),
-        Cmd::ButtonRelease(Button::Left),
-        Cmd::Sleep(600),
-
-        Cmd::MouseMove(1500, -250),
-        Cmd::ButtonPress(Button::Left),
-        Cmd::Sleep(800),
-        Cmd::ButtonRelease(Button::Left),
-        Cmd::Sleep(600),
-
-        Cmd::Tap(Key::R),
-        Cmd::Sleep(1500),
+        Cmd::Tap(Key::Num2), Cmd::Wait(700),
         
-        Cmd::Tap(Key::W),           // Forward
-        Cmd::Sleep(100),
-        Cmd::Engage(Key::S, 700),   // Back
-    ]).await;
+        Cmd::MouseMove(-3000, 500),
+        Cmd::MouseHold(Button::Left, 850), Cmd::Wait(600),
 
-    // Swap between 1 and 3
+        Cmd::MouseMove(1500, -250),
+        Cmd::MouseHold(Button::Left, 850), Cmd::Wait(600),
+
+        Cmd::MouseMove(1500, -250),
+        Cmd::MouseHold(Button::Left, 850), Cmd::Wait(600),
+
+        Cmd::Tap(Key::R), Cmd::Wait(1500),
+        Cmd::Tap(Key::W), Cmd::Wait(100),
+        Cmd::Hold(Key::S, 700),   // Back
+    ], true).await;
+
     let afk3 = macron::new(vec![
-        Cmd::MouseMove(0, 24000),
-        Cmd::Sleep(50),
-        Cmd::MouseMove(0, -14500),
-        Cmd::Sleep(50),
+        Cmd::MouseMove(0, 24000), Cmd::Wait(50),
+        Cmd::MouseMove(0, -14500), Cmd::Wait(50),
 
         // 1
         Cmd::Tap(Key::Num1),
-        Cmd::ButtonPress(Button::Right),
-        Cmd::Sleep(900),
-        Cmd::Click(Button::Left),
-        Cmd::ButtonRelease(Button::Right),
+        Cmd::MousePress(Button::Right),
+        Cmd::Wait(900),
+        Cmd::MouseClick(Button::Left),
+        Cmd::MouseRelease(Button::Right),
 
         // walk
-        Cmd::Tap(Key::W),           // Forward
-        Cmd::Sleep(100),
-        Cmd::Engage(Key::S, 500),   // Back
-        Cmd::Sleep(800),
+        Cmd::Tap(Key::W), Cmd::Wait(100),
+        Cmd::Hold(Key::S, 500), Cmd::Wait(800),
 
         // 2
-        Cmd::Tap(Key::Num2),
-        Cmd::Sleep(900),
-        Cmd::MouseMove(0, 24000),
-        Cmd::Sleep(50),
-        Cmd::MouseMove(0, -15000),
-        Cmd::Sleep(50),
-        Cmd::ButtonPress(Button::Left),
-        Cmd::Sleep(800),
-        Cmd::ButtonRelease(Button::Left),
-
-        Cmd::Tap(Key::W),           // Forward
-        Cmd::Sleep(100),
-        Cmd::Engage(Key::S, 400),   // Back
-        Cmd::Sleep(200),
+        Cmd::Tap(Key::Num2), Cmd::Wait(900),
+        Cmd::MouseMove(0, 24000), Cmd::Wait(50),
+        Cmd::MouseMove(0, -15000), Cmd::Wait(50),
+        Cmd::MouseHold(Button::Left, 800),
 
         // walk
-        Cmd::Tap(Key::W),           // Forward
-        Cmd::Sleep(100),
-        Cmd::Engage(Key::S, 400),   // Back
-        Cmd::Sleep(200),
-    ]).await;
+        Cmd::Tap(Key::W), Cmd::Wait(100),
+        Cmd::Hold(Key::S, 400), Cmd::Wait(200),
+    ], true).await;
 
     let mut key_rx = winky::listen();
     loop {
         tokio::select! {
             Some((code, down)) = key_rx.recv() => {
-                //println!("{:?}, {}", code, down);
                 if code == Key::F9 as u32 && down {
-                    println!("Toggling macro");
                     let _ = afk1.toggle();
                 }
                 else if code == Key::F10 as u32 && down {
-                    println!("Toggling macro");
                     let _ = afk2.toggle();
                 }
                 else if code == Key::F11 as u32 && down {
-                    println!("Toggling macro");
                     let _ = afk3.toggle();
                 }
             }
